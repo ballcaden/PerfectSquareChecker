d51a3636be5998b40163f6fa29aa385e7dac3a20
ballcaden <ballcaden@gmail.com>
2023-11-08 09:24:30 -0500

    init

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+            int input;
+            boolean output = false;
+
+            System.out.println("Enter a number: ");
+            input = scan.nextInt();
+
+            for (int i = 1; i <= input; i++) {
+                if(i * i == input) {
+                    output = true;
+                    break;
+                }
+            }
+
+
+            System.out.println("Output: " + output);
+    }
+}
